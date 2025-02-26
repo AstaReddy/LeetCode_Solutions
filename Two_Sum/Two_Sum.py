36ecc920b49ea68cad5aa70e263ef3ba4fff3b53
AstaReddy <astareddy714@gmail.com>
2025-02-26 14:21:46 -0500

    Removed TwoSum solution

--- Two_Sum/Two_Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        new_map = {}
-        for index, value in enumerate(nums):
-            diff = target - value
-            if diff in new_map:
-                return [new_map[diff],index]
-            new_map[value] = index
-        
