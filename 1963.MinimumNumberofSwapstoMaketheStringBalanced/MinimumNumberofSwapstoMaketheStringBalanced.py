cdc959de33f834ee3c92a36bd6c5a851f5e37966
AstaReddy <astareddy714@gmail.com>
2025-06-11 09:19:10 -0400

    Solution to 1963

---
+++ 1963.MinimumNumberofSwapstoMaketheStringBalanced/MinimumNumberofSwapstoMaketheStringBalanced.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        count,maxcount = 0,0
+
+        for c in s:
+            if c == '[':
+                count-=1
+            else:
+                count+=1
+            maxcount = max(count,maxcount)
+        return((maxcount+1)//2)
+        
