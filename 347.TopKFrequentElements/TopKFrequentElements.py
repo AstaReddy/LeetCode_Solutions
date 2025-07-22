931549ff3a13fe420911b14b4309760e1dd227c9
AstaReddy <astareddy714@gmail.com>
2025-07-22 10:14:32 -0400

    Edit to solution 347

--- 347.TopKFrequentElements/TopKFrequentElements.py
+++ 347.TopKFrequentElements/TopKFrequentElements.py
@@ -1,19 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        result = {}
-        ans = []
-        for i in nums:
-            if i not in result:
-                result[i] = 1
-            else:
-                result[i] += 1
-        sorted_result = dict(sorted(result.items(),key = lambda x : x[1],reverse = True))
-        for key in sorted_result.keys():
-            if len(ans)>k-1:
-                return ans
-            else:
-                ans.append(key)
-        return(ans)
-
-
+        res = Counter(nums)
+        final = []
+        for i in range(k):
+            max_num = max(res,key=res.get)
+            final.append(max_num)
+            del res[max_num]
+        return(final)
+            
         
